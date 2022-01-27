53efa5972d7f52f75a23245eea6d2940a823b2c0
LoloziAnas <lolozianas1@gmail.com>
2022-01-27 18:59:32 +0000

    Implementing instrumentation tests for words app

--- WordsApp/app/src/androidTest/java/com/lolozianas/wordsapp/ExampleInstrumentedTest.kt
+++ WordsApp/app/src/androidTest/java/com/lolozianas/wordsapp/ExampleInstrumentedTest.kt
@@ -1,18 +1,26 @@
 package com.lolozianas.wordsapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+// RunWith annotation is to specify the test runner
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
@@ -20,5 +28,24 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.lolozianas.wordsapp", appContext.packageName)
+    }
+
+    // launch to main activity to the test
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+
+    @Test
+    fun navigate_to_word() {
+        // click on an element has the text C
+        onView(withText("C")).perform(click())
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                2,
+                click()
+            )
+        )
+        onView(withText("Words That Start With C")).check(matches(isDisplayed()))
     }
 }

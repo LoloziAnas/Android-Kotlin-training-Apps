aca162e44c5e470ca9fe4c03ecd81d167ae686a1
LoloziAnas <lolozianas1@gmail.com>
2022-02-03 04:17:47 +0000

    finishing Unscramble App

--- UnscrambleApp/app/src/main/java/com/lolozianas/unscrambleapp/ui/game/GameFragment.kt
+++ UnscrambleApp/app/src/main/java/com/lolozianas/unscrambleapp/ui/game/GameFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.lolozianas.unscrambleapp.R
 import com.lolozianas.unscrambleapp.databinding.GameFragmentBinding
 
 /** [GameFragment] a fragment contain the game logic*/
@@ -20,7 +22,7 @@ class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the XML file and return a binding object instance
         binding = GameFragmentBinding.inflate(inflater, container, false)
         return binding.root
@@ -28,23 +30,87 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        // Set the viewModel for data binding - this allows the bound layout access
+        // to all the data in the VieWModel
+        binding.gameViewModel = viewModel
+        binding.maxNoOfWords = MAX_NO_OF_WORDS
+        // Specify the fragment view as the lifecycle owner of the binding.
+        // This is used so that the binding can observe LiveData updates
+        binding.lifecycleOwner = viewLifecycleOwner
         // Setup a listeners click for skip and submit buttons
         binding.buttonSkip.setOnClickListener { onSkipWord() }
         binding.buttonSubmit.setOnClickListener { onSubmitWord() }
     }
 
     private fun onSubmitWord() {
-        TODO("Not yet implemented")
+        val playerWord = binding.inputEditTextWord.text?.trim().toString()
+        if (viewModel.isUserCurrentWord(playerWord)) {
+            setErrorTextField(false)
+            binding.inputEditTextWord.text?.clear()
+            if (!viewModel.nextWord()) {
+                showFinalScoreDialog()
+            }
+        } else {
+            setErrorTextField(true)
+        }
+
     }
 
+    /* Create and show a dialog alert with the final score*/
+    private fun showFinalScoreDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            // Set the alert dialog's title
+            .setTitle(getString(R.string.congratulations))
+            // Set message that containing the final score of the player
+            .setMessage(getString(R.string.you_scored, viewModel.score.value))
+            .setCancelable(false)
+            // Restart the game of the player click on the positive button
+            .setPositiveButton(getString(R.string.play_again)) { _, _ -> restartGame() }
+            // Exits the game of the player clicks on the negative button
+            .setNegativeButton(getString(R.string.exit)) { _, _ -> exitGame() }
+            // Show the dialog
+            .show()
+    }
+
+    /* Exits the game */
+    private fun exitGame() {
+        activity?.finish()
+    }
+    /*
+     * Re-initializes the data in the ViewModel and updates the views with the new data, to
+     * restart the game.
+     */
+
+    private fun restartGame() {
+        viewModel.reinitializeData()
+        setErrorTextField(false)
+    }
+
+    /*
+    * Skips the current word without changing the score.
+    * Increases the word count.
+    * After the last word, the user is shown a Dialog with the final score.
+    */
     private fun onSkipWord() {
-        TODO("Not yet implemented")
+        if (viewModel.nextWord()) {
+            setErrorTextField(false)
+        } else {
+            showFinalScoreDialog()
+        }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
+    /*
+      * Sets and resets the text field error status.
+      */
+    private fun setErrorTextField(error: Boolean) {
+        if (error) {
+            binding.inputLayout.isErrorEnabled = true
+            binding.inputLayout.error = getString(R.string.try_again)
+        } else {
+            binding.inputLayout.isErrorEnabled = false
+            binding.inputLayout.error = null
+        }
     }
+
 
 }
--- UnscrambleApp/app/src/main/java/com/lolozianas/unscrambleapp/ui/game/GameViewModel.kt
+++ UnscrambleApp/app/src/main/java/com/lolozianas/unscrambleapp/ui/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.lolozianas.unscrambleapp.ui.game
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -17,6 +18,81 @@ class GameViewModel : ViewModel() {
     private var _currentWordCount = MutableLiveData(0)
     val currentWordCount: LiveData<Int> get() = _currentWordCount
 
-    private var _currentScrambleWord = MutableLiveData("test")
+    private var _currentScrambleWord = MutableLiveData<String>()
     val currentScrambleWord: LiveData<String> get() = _currentScrambleWord
+
+    private lateinit var currentWord: String
+
+    // Hold the words that already used in the game
+    private var wordsList: MutableList<String> = mutableListOf()
+
+    init {
+        getNextWord()
+    }
+
+    /*
+     * Updates currentWord and currentScrambledWord with the next word.
+     */
+    private fun getNextWord() {
+        // Gets a random word from the words list
+        currentWord = allWordsList.random()
+        // Convert the word to array's char
+        val tempWord = currentWord.toCharArray()
+        // Shuffled the array
+        tempWord.shuffle()
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        // Test if the word if already in the words List
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            Log.d("Unscramble", "getNextWord: currentWord: $currentWord")
+            // Sets the shuffled Word
+            _currentScrambleWord.value = String(tempWord)
+            // Increment the Word count
+            _currentWordCount.value = _currentWordCount.value?.inc()
+            // Add the currentWord to words List that already displayed
+            wordsList.add(currentWord)
+        }
+    }
+
+    /*
+    * Return true if the player's word is correct
+    * And increment the score accordingly
+    * */
+    fun isUserCurrentWord(playerWord: String): Boolean {
+        return if (playerWord.equals(currentWord, false)) {
+            increaseScore()
+            true
+        } else false
+    }
+
+    /*
+    * Increase the game's score if the player's word correct
+    * */
+    private fun increaseScore() {
+        _score.value = _score.value?.plus(SCORE_INCREASE)
+    }
+
+    /*
+    * Return true if the current word count is less the MAX_NO_OF_WORDS
+    * */
+    fun nextWord(): Boolean {
+        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    /*
+      re-initialize the game data to restart the game
+      */
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
+    }
+
 }
